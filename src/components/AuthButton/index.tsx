40aa507f6126ca0c1fca3d026ad35e0a2725c4f5
Jason Everton <jasonemsw10@gmail.com>
2023-05-23 16:51:44 -0300

    Second commit

--- src/components/AuthButton/index.tsx
+++ src/components/AuthButton/index.tsx
@@ -4,12 +4,15 @@ export const AuthButtons = () => {
   return (
     <Container>
       <AuthButton>
+        <img src="/images/icons/google.svg" alt="google_logo" />
         Entrar com Google
       </AuthButton>
       <AuthButton>
+        <img src="/images/icons/github.svg" alt="github_logo" />
         Entrar com Github
       </AuthButton>
       <AuthButton>
+        <img src="/images/icons/rocket.svg" alt="rocket_icon" />
         Entrar como visitante
       </AuthButton>
     </Container>
